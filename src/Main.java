e5a505c45d5c58b0926e31cb544d306b4a384973
RUDNEY LOPES DE SOUZA JUNIOR <rudney.lsjunior@senacsp.edu.br>
2025-05-22 18:06:08 -0300

    Inicio Hora de Codar Java

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,50 @@
+
+
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        //ex01
+        exercicio3();
 
+
+    }
+
+    public static void exercicio1(){
+        //ex01
         String planeta = "Plutão";
         // Cria a variável 'planeta' e atribui o valor "Plutão"
 
         System.out.println("O planeta é: " + planeta);
         // Exibe o valor da variável
+    }
 
+    public static void exercicio2(){
         //ex02
         Scanner entrada = new Scanner(System.in);
         String nome = "Exemplo";
+        // Cria a variável 'planeta' e atribui o valor "Plutão"
         System.out.println("Qual é o seu Nome?");
         nome = entrada.nextLine(); //Altamente tipado
         System.out.println("Olá, " + nome);
+        // Exibe o valor da variável
+
         entrada.close();
 
+    }
+    public static void exercicio3(){
         //ex03
         Scanner entrada = new Scanner(System.in);
-        String nome = "Exemplo";
+
         System.out.println("Qual é o seu Nome?");
-        nome = entrada.nextLine(); //Altamente tipado
-        int= 0;
-        System.out.println("Qual é o seu Nome?");
+        String nome = entrada.nextLine(); //Altamente tipado
+        System.out.println("Qual é a sua idade?");
+        int idade = entrada.nextInt();
+        // Cria a variável 'planeta' e atribui o valor "Plutão"
+
+
         System.out.println("Olá, " + nome);
-        entrada.close();
-
-
-
-
-
+        System.out.print("sua idade é " + idade);
+        // Exibe o valor da variável
 
     }
 }
